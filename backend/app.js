a5a9a0a9eeff05fa21240a34dc55a013e1568121
AmanTrivedi1 <amantrivedi598@gmail.com>
2023-07-05 17:16:05 +0530

    auth completed

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const errorMiddleware = require("./middleware/error");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(express.json());
-
+app.use(cookieParser());
 // Route Imports
 const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
 
 app.use("/api/v1", product);
+app.use("/api/v1", user);
 
 // Middleware for Errors
 app.use(errorMiddleware);
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,17 +6,20 @@ const {
   deleteProduct,
   getProductDetails,
 } = require("../controllers/productController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 
-router.route("/product/new").post(createProduct);
+router
+  .route("/product/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
 
 router
   .route("/product/:id")
-  .put(updateProduct)
-  .delete(deleteProduct)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct)
   .get(getProductDetails);
 
 module.exports = router;
